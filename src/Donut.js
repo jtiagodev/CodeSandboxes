7f4bab47eafc902dfe810475c5477812756ba527
João Tiago <joao_t332@hotmail.com>
2020-07-17 16:53:10 +0100

    Update Donut.js

--- src/Donut.js
+++ src/Donut.js
@@ -5,9 +5,13 @@ import { extendDefaultDoughnut } from "./utils";
 extendDefaultDoughnut();
 
 const Donut = props => {
-  const { data, options } = props;
-
-  return <Doughnut data={data} options={options} />;
+  const { data, options, doughnutWidth = 300, doughtnutHeight = 300 } = props;
+  
+  return <Doughnut 
+  data={data} 
+  width={doughnutWidth}
+  height={doughnutHeight}
+  options={options} />;
 };
 
 export default Donut;
